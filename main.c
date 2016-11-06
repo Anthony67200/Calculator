213aca96023007839cd6227ba07e46f13fea2b61
Anthony DE CAIRES <decaires.anthony@gmail.com>
2016-11-06 21:26:54 +0100

    Soustraction

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition(int a , int b)
-{
-    return a + b;
-}
 
 int soustraction (int a , int b)
 {
@@ -14,8 +10,7 @@ int soustraction (int a , int b)
 
 int main()
 {
-    printf("L'addition de a + b vaut %d\n", addition(1,4));
-    return 0;
+
     printf("La soustraction de a -b vaut %d\n", soustraction (1,4));
     return 0;
 }
